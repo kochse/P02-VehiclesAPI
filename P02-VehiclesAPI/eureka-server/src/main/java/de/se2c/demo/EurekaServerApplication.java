9f7470e90ef83ff1ec903736c0f8078108917da8
Sebastian Koch <kochsebastian@gmail.com>
2021-12-07 16:47:44 +0100

    add eureka server

---
+++ P02-VehiclesAPI/eureka-server/src/main/java/de/se2c/demo/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package de.se2c.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
---
+++ P02-VehiclesAPI/eureka-server/src/test/java/de/se2c/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.se2c.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
