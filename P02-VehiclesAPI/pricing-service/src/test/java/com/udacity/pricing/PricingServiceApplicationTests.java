4bc1f6d9a7d5326c2042ba1ad361de502ac66489
Sebastian Koch <kochsebastian@gmail.com>
2021-12-09 19:11:46 +0100

    add pricing service test

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,39 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.api.PricingController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.containsString;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+	@Autowired
+	private PricingController controller;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	public void shouldReturnVehiclePrice() throws Exception {
+		this.mockMvc.perform(get("/services/price?vehicleId=1")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("\"vehicleId\":1}")));
 	}
 
 }
